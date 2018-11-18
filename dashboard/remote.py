aff5505c2dbc3328e3d50964a19defe05e8feb2d
George <ggalan87@gmail.com>
2018-11-18 14:27:04 +0200

    Use lorem library for sample/placeholder text

--- dashboard/remote.py
+++ dashboard/remote.py
@@ -1,18 +1,12 @@
 from collections import namedtuple
+import lorem
 
 Show = namedtuple('Show', ['title', 'users', 'nickname', 'description', 'sites', 'logo_path'])
 
 
 class Store:
 
-    sample_descr = \
-        """
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
-        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
-        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        """
+    sample_descr = lorem.paragraph()
 
     shows = \
     [
