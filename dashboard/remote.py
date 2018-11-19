dc00e9df82ac5082955e181625991cd2208cc5d2
George <ggalan87@gmail.com>
2018-11-19 02:23:02 +0200

    query scheduler for remaining time

--- dashboard/remote.py
+++ dashboard/remote.py
@@ -1,8 +1,9 @@
-from collections import namedtuple
 import lorem
+from dashboard.models import Show
 
-Show = namedtuple('Show', ['title', 'users', 'nickname', 'description', 'sites', 'logo_path'])
-
+import requests
+import math
+import json
 
 class Store:
 
@@ -11,8 +12,8 @@ class Store:
     shows = \
     [
         Show('Rastamidnights', ['arouraios'], 'Jerry', sample_descr, ['https://radio.uoc.gr'], ''),
-        Show('Awsome show description', ['arouraios'], 'Jerry', sample_descr, ['https://radio.uoc.gr'], ''),
-        Show('Awkward show description', ['arouraios'], 'Jerry', sample_descr, ['https://radio.uoc.gr'], ''),
+        Show('Awsome show title', ['arouraios'], 'Jerry', sample_descr, ['https://radio.uoc.gr'], ''),
+        Show('Awkward show title', ['ggalan'], 'Jerry', sample_descr, ['https://radio.uoc.gr'], ''),
     ]
 
     def get_user_shows(self, username):
@@ -24,5 +25,35 @@ class Store:
 
 
 def validate_login(username, password):
-    #return username == 'arouraios' and password == 'papakia'
-    return not (username + password)
+    return username == 'arouraios' and password == 'papakia'
+    #return not (username + password)
+
+
+def get_metadata():
+    try:
+        r = requests.get('http://prod.radio.uoc.gr:9670/')
+    except requests.exceptions.RequestException as e:  # This is the correct syntax
+        # Awkward way to get the errno from the error message, because it is not stored in the object, unlike older
+        # version of the library as reported in:
+        # https://stackoverflow.com/questions/19370436/get-errno-from-python-requests-connectionerror
+        # msg = e.args[0].reason.args[0]
+        # errno = int(re.search(r"\[Errno\ ([0-9]+)\]", msg).group(1))
+        # exit(errno)
+        return None, 15
+
+    try:
+        data = json.loads(r.text)
+    except json.decoder.JSONDecodeError:
+        print(r.text)
+        raise Exception("Strange JSON {}".format(r.text))
+
+    duration = int(data['current_song']['Duration'])
+    elapsed = int(data['current_song']['Elapsed'])
+
+    return data, (duration - elapsed)
+
+
+def query_autopilot_remaining():
+    data, next = get_metadata()
+    print(data)
+    return next
